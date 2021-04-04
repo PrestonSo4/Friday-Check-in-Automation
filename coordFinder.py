5a92de0beb3b8582a2bb99612cfd578fc922e06f
prestonchso <prestonchso@gmail.com>
2021-04-04 16:15:23 -0700

    more stuff

--- coordFinder.py
+++ coordFinder.py
@@ -1,8 +1,3 @@
-'''
-Since the coords for PYautoGUI differ for each OS, I created this program to easily find coords.
-'''
-import pyautogui, time
-print("Press \"CTRL + C\" when you want to quit the program")
-while True:
-    print(pyautogui.position())
-    time.sleep(1.5)
+from datetime import date
+
+print(date.today())
