2a4248ce65eaad7c1a9de0e30cc4d1c9f432e942
prestonchso <prestonchso@gmail.com>
2021-04-04 13:22:56 -0700

    fixing bugs

--- main.py
+++ main.py
@@ -18,16 +18,18 @@ def moveClick(x,y, time):
     time.sleep(0.5)
     pClick()
 def sendText(xpath, text):
+    import time
+    time.sleep(2)
     driver.find_element_by_xpath(xpath).send_keys(text)
 def zoom(iterations, symbol):
     p.keyDown('ctrl')
     for i in range(iterations):
         p.press(symbol)
     p.keyUp('ctrl')
-def clickImage(file):
+def clickImage(file, confidence):
     import time
     time.sleep(1)
-    image = p.locateOnScreen(file)
+    image = p.locateOnScreen(file, confidence = confidence)
     imageX, imageY = p.center(image)
     p.click(imageX, imageY)
     time.sleep(1)
@@ -39,7 +41,7 @@ moveClick(1248,450,.5)
 sendText('//*[@id="ctl00_MainContent_username"]', un)
 sendText('//*[@id="ctl00_MainContent_password"]', pw)
 driver.find_element_by_xpath('//*[@id="ctl00_MainContent_Submit1"]').click()
-clickImage('Grade_Book_Button.png')
+clickImage('Grade_Book_Button.png',0.9)
 zoom(2, '-')
 time.sleep(2)
 loc = p.locateOnScreen('Grades.png', confidence=0.6) #I'm using the locate image function so that the coords wont have to change depending on screensize
@@ -57,9 +59,11 @@ p.drag(513,581, 1, button='left') #Dragging to the specified screenshot size
 #screenshot coords: move right - 513,move down - 581
 driver.get('https://bsd405.sharepoint.com/:o:/r/sites/Section_df6688a0-c5d8-4d46-9b97-ccca24375f14/_layouts/15/Doc.aspx?sourcedoc=%7BEB5E2917-A691-40E9-8E8B-0A2CDCA5030C%7D&file=2020-FY-P7-Advisory%203%20Notebook&action=edit&mobileredirect=true&wdorigin=Sharepoint&RootFolder=%2Fsites%2FSection_df6688a0-c5d8-4d46-9b97-ccca24375f14%2FSiteAssets%2F2020-FY-P7-Advisory%203%20Notebook&web=1&ct=1617479094686&wdOrigin=OFFICECOM-WEB.START.MRU&cid=2bfb33b6-e868-42dc-9703-cd714cd75f18')
 sendText('//*[@id="i0116"]', email)
-clickImage('Next_Sign_in_Button.png')
+time.sleep(1)
+clickImage('Next.png',0.9)
 sendText('//*[@id="i0118"]', pw)
-p.click()
+clickImage('Sign_in.png',0.9)
+clickImage('Yes.png', 0.8)
 """
 --LAYOUT--
 
@@ -71,10 +75,7 @@ Clicks 'Gradebook' button ✔
 Zooms out to 80% Zoom ✔
 Presses keys: 'CTRL+SHIFT+S' ✔
 Takes screenshot at specific coords (I have to get pyautogui to hold mouse down) ✔
-Goes to: https://bsd405.sharepoint.com/:o:/r/sites/Section_df6688a0-c5d8-4d46-9b97-ccca24375f14/_layouts/15/Doc.aspx?sourcedoc=%7BEB5E2917-A691-40
-E9-8E8B-0A2CDCA5030C%7D&file=2020-FY-P7-Advisory%203%20Notebook&action=edit&mobileredirect=true&wdorigin=Sharepoint&RootFolder=%2Fsites%2FSection_
-df6688a0-c5d8-4d46-9b97-ccca24375f14%2FSiteAssets%2F2020-FY-P7-Advisory%203%20Notebook&web=1&ct=1617479094686&wdOrigin=OFFICECOM-WEB.START.MRU&cid
-=2bfb33b6-e868-42dc-9703-cd714cd75f18 ✔
+Goes to: https://bsd405.sharepoint.com/:o:/r/sites/Section_df6688a0-c5d8-4d46-9b97-ccca24375f14/_layouts/15/Doc.aspx?sourcedoc=%7BEB5E2917-A691-40E9-8E8B-0A2CDCA5030C%7D&file=2020-FY-P7-Advisory%203%20Notebook&action=edit&mobileredirect=true&wdorigin=Sharepoint&RootFolder=%2Fsites%2FSection_df6688a0-c5d8-4d46-9b97-ccca24375f14%2FSiteAssets%2F2020-FY-P7-Advisory%203%20Notebook&web=1&ct=1617479094686&wdOrigin=OFFICECOM-WEB.START.MRU&cid=2bfb33b6-e868-42dc-9703-cd714cd75f18 ✔
 Look for the 'So, Preston D (Student)' tab
 Click on 'So, Preston D (Student) tab 
 Look for the 'Friday Check-in' button 
